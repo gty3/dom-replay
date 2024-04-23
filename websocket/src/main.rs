37e1ad02abd422bcec27347f1a7e9f1e16e49d75
gty <gefyoung@gmail.com>
2024-04-23 18:58:39 +0100

    cargo lambda new

---
+++ websocket/src/main.rs
@@ -0,0 +1,21 @@
+use aws_lambda_events::event::connect::ConnectEvent;
+use lambda_runtime::{run, service_fn, tracing, Error, LambdaEvent};
+
+
+/// This is the main body for the function.
+/// Write your code inside it.
+/// There are some code example in the following URLs:
+/// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
+/// - https://github.com/aws-samples/serverless-rust-demo/
+async fn function_handler(event: LambdaEvent<ConnectEvent>) -> Result<(), Error> {
+    // Extract some useful information from the request
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    tracing::init_default_subscriber();
+
+    run(service_fn(function_handler)).await
+}
