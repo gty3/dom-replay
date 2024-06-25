84f13ab1ef3d01af1948fb97c2767861defecb68
gty <gefyoung@gmail.com>
2024-06-25 14:00:58 +0000

    added a middleware file to forward to start time, didn't work

--- nextjs/next.config.mjs
+++ nextjs/next.config.mjs
@@ -1,4 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/cl', // Replace with your default symbol page
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
