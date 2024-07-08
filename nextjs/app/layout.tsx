fc6951e39cfc5440f0c213d3a32eb10f4807db57
gty3 <gefyoung@gmail.com>
2024-07-08 02:39:30 +0000

    order Flow

--- nextjs/app/layout.tsx
+++ nextjs/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "Order flow",
+  title: "Order Flow",
   description: "Depth of Market Simulator using historical data",
 }
 
