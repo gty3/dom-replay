46eb67ba5e2db2c90a18b2e83709828f0b2518e0
gty3 <gefyoung@gmail.com>
2024-07-08 02:39:16 +0000

    changed title to Order flwo

--- nextjs/app/layout.tsx
+++ nextjs/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "DOM Replay",
+  title: "Order flow",
   description: "Depth of Market Simulator using historical data",
 }
 
