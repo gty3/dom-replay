d15217bc7695d9aac55890e755f3e9a3b40aa09b
gty3 <gefyoung@gmail.com>
2024-07-30 22:35:14 +0000

    maybe revert send_data.rs

--- websocket/src/handle_default/send_data.rs
+++ websocket/src/handle_default/send_data.rs
@@ -5,6 +5,7 @@ use tokio::sync::mpsc::Receiver;
 use tokio::sync::oneshot;
 use tokio::time::timeout;
 use tokio::time::Duration;
+use std::sync::{Arc, atomic::{AtomicBool, Ordering}};
 
 pub async fn send_data(
     apigateway_client: &Client,
@@ -12,16 +13,21 @@ pub async fn send_data(
     mut message_rx: Receiver<(u64, String)>,
     replay_start: time::OffsetDateTime,
     mut wait_for_initial: bool,
-    // cancel_flag: Arc<AtomicBool>,
     mut cancel_rx: oneshot::Receiver<()>,
 ) -> Result<(), Error> {
 
     let start_time = tokio::time::Instant::now();
     let replay_start_nanos = replay_start.unix_timestamp_nanos() as u64;
+    let error_flag = Arc::new(AtomicBool::new(false));
 
     loop {
         tokio::select! {
             Some((current_ts, message)) = message_rx.recv() => {
+                if error_flag.load(Ordering::Relaxed) {
+                    println!("Error flag set, stopping message processing",);
+                    break;
+                }
+
                 let target_time = current_ts.saturating_sub(replay_start_nanos);
                 let elapsed = start_time.elapsed().as_nanos() as u64;
 
@@ -42,6 +48,7 @@ pub async fn send_data(
                 // Send the message without waiting for response
                 let client = apigateway_client.clone();
                 let connection_id = connection_id.to_string();
+                let error_flag = Arc::clone(&error_flag);
 
                 tokio::spawn(async move {
                     match client
@@ -52,7 +59,10 @@ pub async fn send_data(
                         .await
                     {
                         Ok(_) => {},
-                        Err(e) => println!("Error sending message: {:?}", e),
+                        Err(e) => {
+                            println!("Error sending message: {:?}", e);
+                            error_flag.store(true, Ordering::Relaxed);
+                        }
                     }
                 });
             }
