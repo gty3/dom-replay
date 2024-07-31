b3f5db9b200d5da04af3edeae4fccd2817f88fc2
gty3 <gefyoung@gmail.com>
2024-07-31 20:38:27 +0000

    added touch scroll

--- nextjs/app/[symbol]/hooks/useDomScroll.ts
+++ nextjs/app/[symbol]/hooks/useDomScroll.ts
@@ -1,7 +1,9 @@
-import { useEffect } from "react"
+import { useEffect, useRef } from "react"
 import { ReducerAction } from "../../types"
 
 const useDomScroll = (increment: number, dispatch: React.Dispatch<ReducerAction>) => {
+  const touchStartY = useRef<number | null>(null);
+
   useEffect(() => {
     const handleWheel = (event: WheelEvent) => {
       if (event.deltaY < 0) {
@@ -12,9 +14,34 @@ const useDomScroll = (increment: number, dispatch: React.Dispatch<ReducerAction>
     }
     window.addEventListener("wheel", handleWheel)
 
-    // Cleanup function to remove the event listener
+    const handleTouchStart = (event: TouchEvent) => {
+      touchStartY.current = event.touches[0].clientY;
+    };
+
+    const handleTouchMove = (event: TouchEvent) => {
+      if (touchStartY.current === null) return;
+
+      const touchEndY = event.touches[0].clientY;
+      const deltaY = touchEndY - touchStartY.current;
+
+      if (Math.abs(deltaY) >= 32) {
+        if (deltaY < 0) {
+          dispatch({ type: "SCROLL_DOWN", payload: increment });
+        } else {
+          dispatch({ type: "SCROLL_UP", payload: increment });
+        }
+        touchStartY.current = touchEndY;
+      }
+    };
+
+    window.addEventListener("touchstart", handleTouchStart);
+    window.addEventListener("touchmove", handleTouchMove);
+
+    // Cleanup function to remove the event listeners
     return () => {
       window.removeEventListener("wheel", handleWheel)
+      window.removeEventListener("touchstart", handleTouchStart);
+      window.removeEventListener("touchmove", handleTouchMove);
     }
   }, [dispatch, increment])
 }
